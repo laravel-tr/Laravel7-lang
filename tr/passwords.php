30d7180a553afcb31d013a0d0b4627a410e06262
Sinan Eldem <sinan@sinaneldem.com.tr>
2020-03-17 18:46:27 +0300

    Laravel 7 Dil Dosyaları eklendi

---
+++ tr/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Parola hatırlatma metinleri
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler, başarısız olmuş bir parola sıfırlama
+    | işleminde oluşan hataları temsil eden mesajlardır.
+    |
+    */
+
+    'reset' => 'Parolanız sıfırlandı!',
+    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'token' => 'Parola sıfırlama adresi/kodu geçersiz.',
+    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+];
